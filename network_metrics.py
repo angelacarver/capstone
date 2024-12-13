0a3d4ab7e3d71544e60aa61fed2e6423982bc931
Angela <none>
2024-12-12 18:51:35 -0600

    finished with bulk of metrics py

--- network_metrics.py
+++ network_metrics.py
@@ -12,6 +12,9 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.metrics import jaccard_score
 from sklearn.neighbors import NearestNeighbors
+import pickle
+from sklearn.metrics.pairwise import cosine_similarity
+from tqdm import tqdm
 
 """
 This python file contains methods for computing GRN network similarity metrics.
@@ -161,7 +164,50 @@ def plot_network(network, title):
     plt.title(title)
     plt.show()
 
+def embeddings_to_networkx(network_embeddings):
+    reconstructed_adj = network_embeddings @ network_embeddings.T  # Dot product to get similarity scores
 
+    # Threshold to create edges (e.g., values > 0.5 are considered connected)
+    threshold = 0.5
+    reconstructed_adj_binary = (reconstructed_adj > threshold).astype(int)
+
+    # Convert adjacency matrix to a NetworkX graph
+    G = nx.from_numpy_array(reconstructed_adj_binary)
+    return G
+
+def corr_to_networkx(corr_matrix, threshold=0.5):
+    adj_matrix = np.where(np.abs(corr_matrix) > threshold, 1, 0)
+    np.fill_diagonal(adj_matrix, 0)
+    G = nx.from_numpy_array(adj_matrix)
+    #assign correlation values as edge weights
+    for i in tqdm(range(corr_matrix.shape[0])):
+        for j in range(i+1, corr_matrix.shape[1]):
+            if np.abs(corr_matrix[i, j]) > threshold:
+                G.add_edge(i, j, weight=corr_matrix[i, j])
+    
+    return G
+
+def node2vec_to_networkx(network: dict, threshold=0.5):
+    # Convert the embeddings dictionary to a matrix
+    nodes = list(network.keys())
+    vectors = np.array(list(network.values()))
+
+    # Compute pairwise cosine similarity
+    similarity_matrix = cosine_similarity(vectors)
+
+    # Create a graph
+    G = nx.Graph()
+
+    # Add nodes
+    G.add_nodes_from(nodes)
+
+    # Add edges based on similarity threshold
+    for i, node1 in tqdm(enumerate(nodes)):
+        for j, node2 in enumerate(nodes):
+            if i < j and similarity_matrix[i, j] > threshold:
+                G.add_edge(node1, node2, weight=similarity_matrix[i, j])
+
+    return G
 
 
 if __name__ == '__main__':
@@ -170,13 +216,107 @@ if __name__ == '__main__':
     Comparing models
     """    
     #read in data
-    
+    #the Pearson Correlation coefficient networks are already networkx objects
+    with open('corr_control_graph.pickle', 'rb') as f:
+        corr_control = pickle.load(f)
+    with open('corr_group2_graph.pickle', 'rb') as f:
+        corr_treat1 = pickle.load(f)
+    with open('corr_group3_graph.pickle', 'rb') as f:
+        corr_treat2 = pickle.load(f)
+
+    with open('node2vec_control_embeddings.pickle', 'rb') as f:
+        node2vec_control = pickle.load(f)
+    with open('node2vec_group2_embeddings.pickle','rb') as f:
+        node2vec_treat1 = pickle.load(f)
+    with open('node2vec_group3_embeddings.pickle', 'rb') as f:
+        node2vec_treat2 = pickle.load(f)
+
+    vgae_control_embeddings = np.load("vgae_control_node_embeddings.npy", allow_pickle=True)
+    vgae_treat1_embeddings = np.load("vgae_group2_node_embeddings.npy", allow_pickle=True)
+    vgae_treat2_embeddings = np.load("vgae_group3_node_embeddings.npy", allow_pickle=True)
 
     #convert to networkx objects
+    node2vec_control_network = node2vec_to_networkx(node2vec_control)
+    node2vec_treat1_network = node2vec_to_networkx(node2vec_treat1)
+    node2vec_treat2_network = node2vec_to_networkx(node2vec_treat2)
+
+    vgae_control_network = embeddings_to_networkx(vgae_control_embeddings)
+    vgae_treat1_network = embeddings_to_networkx(vgae_treat1_embeddings)
+    vgae_treat2_network = embeddings_to_networkx(vgae_treat2_embeddings)
 
 
-    #run metrics & visualizations
-    degree_distribution(network1, network2, network3, network1_name: str, network2_name: str, network3_name:str)
+    #compare networks with same treatment
+    f_stat, p_value = degree_distribution(corr_control, node2vec_control_network, vgae_control_network, "Control Pearson correlation", "Control node2vec", "Control VGAE")
+    f_stat, p_value = degree_distribution(corr_treat1, node2vec_treat1_network, vgae_treat1_network, "Treatment 1 Pearson correlation", "Treatment 1 node2vec", "Treatment 1 VGAE")
+    f_stat, p_value = degree_distribution(corr_treat2, node2vec_treat2_network, vgae_treat2_network, "Treatment 2 Pearson correlation", "Treatment 2 node2vec", "Treatment 2 VGAE")
+
+    f_stat, p_value = clustering_coefficients(corr_control, node2vec_control_network, vgae_control_network)
+    f_stat, p_value = clustering_coefficients(corr_treat1, node2vec_treat1_network, vgae_treat1_network)
+    f_stat, p_value = clustering_coefficients(corr_treat2, node2vec_treat2_network, vgae_treat2_network)
+
+    modularity_scores = modularity_score(corr_control, node2vec_control_network, vgae_control_network)
+    modularity_scores = modularity_score(corr_treat1, node2vec_treat1_network, vgae_treat1_network)
+    modularity_scores = modularity_score(corr_treat2, node2vec_treat2_network, vgae_treat2_network)
+
+    print("Node Centrality Between Model Types")
+    node_centrality(corr_control, node2vec_control_network, vgae_control_network, "Control Pearson Correlation", "Control Node2Vec", "Control VGAE")
+    node_centrality(corr_treat1, node2vec_treat1_network, vgae_treat1_network, "Treatment 1 Pearson Correlation", "Treatment 1 Node2Vec", "Treatment 1 VGAE")
+    node_centrality(corr_treat2, node2vec_treat2_network, vgae_treat2_network, "Treatment 2 Pearson Correlation", "Treatment 2 Node2Vec", "Treatment 2 VGAE")
+
+    print("Node Centrality Between Treatments")
+    node_centrality(corr_control, corr_treat1, corr_treat1, "Control Pearson Correlation", "Treatment 1 Pearson Correlation", "Treatment 2 Pearson Correlation")
+    node_centrality(node2vec_control_network, node2vec_treat1_network, node2vec_treat2_network, "Control Node2Vec", "Treament 1 Node2Vec", "Treatment 2 Node2Vec")
+    node_centrality(vgae_control_network, vgae_treat1_network, vgae_treat2_network, "Control VGAE", "Treament 1 VGAE", "Treatment 2 VGAE")
+
+    print('Procrusted Disparity Between Node2Vec and VGAE Embeddings')
+    print('control')
+    procrustes_disparity(node2vec_control, vgae_control_embeddings)
+    print('treat1')
+    procrustes_disparity(node2vec_treat1, vgae_treat1_embeddings)
+    print('treat2')
+    procrustes_disparity(node2vec_treat2, vgae_treat2_embeddings)
+
+    print('Cosine similarity of embeddings')
+    print('control')
+    embedding_cosine_similarity(node2vec_control, vgae_control_embeddings)
+    print('treat1')
+    embedding_cosine_similarity(node2vec_treat1, vgae_treat1_embeddings)
+    print('treat2')
+    embedding_cosine_similarity(node2vec_treat2, vgae_treat2_embeddings)
+
+    print('Jaccard Similarity Between Node2Vec and VGAE')
+    print('control')
+    jaccard_similarity(node2vec_control, vgae_control_embeddings)
+    print('treat1')
+    jaccard_similarity(node2vec_treat1, vgae_treat1_embeddings)
+    print('treat2')
+    jaccard_similarity(node2vec_treat2, vgae_treat2_embeddings)
+
+    print('Plotting Networks')
+    plot_network(corr_control, 'Control Pearson Correlation')
+    plot_network(corr_treat1, 'Treatment 1 Pearson Correlation')
+    plot_network(corr_treat2, 'Treatment 2 Pearson Correlation')
+
+    plot_network(node2vec_control_network, 'Control Node2Vec')
+    plot_network(node2vec_treat1_network, 'Treatment 1 Node2Vec')
+    plot_network(node2vec_treat2_network, 'Treatment 2 Node2Vec')
+
+    plot_network(vgae_control_network, 'Control VGAE')
+    plot_network(vgae_treat1_network, 'Treatment 1 VGAE')
+    plot_network(vgae_treat2_network, 'Treatment 2 VGAE')
+
+
+
+
+    
+
+
+
+
+
+
+
+
 
 
     """
